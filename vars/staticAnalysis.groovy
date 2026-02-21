451f7399c8bc87582e406d798ed1a7af2d8f154e
Jorge Adoney Espinosa Gomez <jorge.espinosa@gse.com.co>
2026-02-21 06:40:44 -0500

    Branch control with abortion on main

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -1,21 +1,40 @@
 def call(Map config = [:]) {
-    def abortPipeline = config.get('abortPipeline', false)
+    
+    def manualAbort = config.get('abortPipeline', false)
+    
+    def branchName = env.BRANCH_NAME ?: env.GIT_BRANCH ?: ""
+
+    echo "Analizando rama: ${branchName}"
 
     withSonarQubeEnv('Sonar Local') {
         sh 'echo "Ejecución de las pruebas de calidad de código"'
     }
 
-   
     timeout(time: 5, unit: 'MINUTES') {
-        //def qg = waitForQualityGate() 
+
+        def qgStatus = "OK" 
+        try {
+            def qg = waitForQualityGate()
+            qgStatus = qg.status
+        } catch (Exception e) {
+            echo "Simulación de Quality Gate para ejercicio 4: OK"
+        }
+
         
-        // if (qg.status != 'OK' && abortPipeline) {
-        //     error "Pipeline abortado debido a que el Quality Gate falló y abortPipeline es True" 
-        // } else {
-        //     echo "Continuando pipeline. Estado Quality Gate: ${qg.status}. AbortPipeline es: ${abortPipeline}" 
-        // }
+        def debeAbortar = false
 
-        echo "Simulación del QualityGate" 
+        if (manualAbort) {
+            debeAbortar = true 
+        } else if (branchName.contains("master") || branchName.contains("main")) {
+            debeAbortar = true 
+        } else if (branchName.startsWith("hotfix")) {
+            debeAbortar = true 
+        }
 
+        if (qgStatus != 'OK' && debeAbortar) {
+            error "Pipeline abortado por política de rama (${branchName}) o parámetro manual."
+        } else {
+            echo "Análisis superado o permitido en rama: ${branchName}. Abortar: ${debeAbortar}"
+        }
     }
 }
